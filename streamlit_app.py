af5d979338d45386632f1d69aed1ed4e8f29bc18
Sebastian Sundet Flaen <sebastiansundetflaen@Sebastians-MacBook-Pro.local>
2022-08-12 17:49:05 +0200

    Oscarsgate test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
-print('hello')
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+
+
+
+json = requests.get('https://visits.evofitness.no/api/v1/locations/1e724456-b640-4b03-9b65-0cf7734aa38e/current').json()
+
+
+st.title(json)
+
+
